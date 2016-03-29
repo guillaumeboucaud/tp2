7ecbc20a20570e5e57b5016a87eefae856cfdbec
Carlos <dvmedellin@hotmail.com>
2016-03-29 13:13:47 -0400

    create database

--- tp2.sql
+++ tp2.sql
@@ -1,1 +1,16 @@
-inf5180 tp2 
+CREATE DATABASE resultats_tp2;
+/
+CREATE TABLE departement (
+   id_dept INTEGER     PRIMARY KEY,
+   nom     VARCHAR(15) NOT NULL
+)
+/
+CREATE TABLE programme (
+   code     INTEGER     PRIMARY KEY,
+   titre    VARCHAR(30) NOT NULL,
+   id_dept  VARCHAR(15) NOT NULL,
+   type     VARCHAR(10),
+   cycle    INTEGER,
+   FOREIGN KEY (id_dept)
+)
+/
