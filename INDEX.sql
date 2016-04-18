fbbd7e1fbfa9db2a7d204ceaa4304e6d9c805f87
guillaumeboucaud <guillaumeboucaud@outlook.com>
2016-04-18 10:13:03 -0400

    Creation des index

--- INDEX.sql
+++ INDEX.sql
@@ -20,7 +20,7 @@
 --        Enseignant.nom,
 --        Enseignant.prenom
 -- FROM   Etudiant
---        JOIN Departement 				ON Etudiant.idProgramme = Departement.idProgramme
+--        JOIN Departement 				ON Etudiant.idProgramme = Programme.idProgramme
 --        JOIN InscriptionGroupeCours 	ON Etudiant.idEtudiant = InscriptionGroupeCours.idEtudiant
 --        JOIN GroupeCours 				ON InscriptionGroupeCours.idGroupeCours = GroupeCours.idGroupeCours
 --        JOIN Cours 					ON GroupeCours.idCours = Cours.idCours
@@ -59,6 +59,24 @@
 --        nip = 'nipEtudiant1' 			AND
 --		  diffusionNotesFinales = 'O'   
 -----------------------------------------------------
+CREATE INDEX idx_Etudiant_Programme			ON Etudiant(idProgramme);
+CREATE INDEX idx_InscriptionGC_Etudiant		ON InscriptionGroupeCours(idEtudiant);
+CREATE INDEX idx_InscriptionGC_GC			ON InscriptionGroupeCours(idGroupeCours);
+CREATE INDEX idx_GroupeCours_Cours			ON GroupeCours(idCours);
+CREATE INDEX idx_GroupeCours_Enseignant		ON GroupeCours(idEnseignant);
+CREATE INDEX idx_GroupeCours_SessionCours	ON GroupeCours(idSessionCours);
+CREATE INDEX idx_ElementsEva_InscriptionGC	ON ElementsEvaluation(idInscriptionGC);
+CREATE INDEX idx_ResultatEva_InscriptionGC	ON ResultatEvaluation(idInscriptionGC);
+CREATE INDEX idx_StatsEva_ElementsEva		ON StatsEvaluation(idElementsEvaluation);
+CREATE INDEX idx_StatsEva_GroupeCours		ON StatsEvaluation(idGroupeCours);
+
+CREATE INDEX idx_Etudiant_nom_prenom_CP 	ON Etudiant(nom, prenom, codePermanent, nip);
+CREATE INDEX idx_Enseignant_nom_prenom  	ON Enseignant(nom, prenom);
+CREATE INDEX idx_Programme_codeNum_titre	ON Programme(titre, codeNumerique);
+CREATE INDEX idx_Cours_sigle_titre			ON Cours(sigleCours, titreCours);
+CREATE INDEX idx_ElementsEva_titre			ON ElementsEvaluation(titreEvaluation);
+CREATE INDEX idx_StatsEva_moy_ecartType		ON StatsEvaluation(moyenne, ecartType);
+CREATE INDEX idx_StatsEva_ElementsEva		ON StatsEvaluation(idElementsEvaluation);
 
 -----------------------------------------------------
 -- Cas 2.5
