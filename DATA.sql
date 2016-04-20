dde1a0aaab7ffb856a400cd6d225db941c754df6
guillaumeboucaud <guillaumeboucaud@outlook.com>
2016-04-19 22:07:21 -0400

    MAJ

--- DATA.sql
+++ DATA.sql
@@ -1,49 +1,47 @@
-SET ECHO ON
-SET SERVEROUTPUT ON
 
 --Recommencer a neuf
-DELETE TABLE Departement;
+TRUNCATE TABLE Departement;
 
-DELETE TABLE Programme;
+TRUNCATE TABLE Programme;
 
-DELETE TABLE Etudiant;
+TRUNCATE TABLE Etudiant;
 
-DELETE TABLE EmployeDepartement;
+TRUNCATE TABLE EmployeDepartement;
 
-DELETE TABLE Enseignant;
+TRUNCATE TABLE Enseignant;
 
-DELETE TABLE SessionCours;
+TRUNCATE TABLE SessionCours;
 
-DELETE TABLE Cours;
+TRUNCATE TABLE Cours;
 
-DELETE TABLE BaremeNoteGroupeCours;
+TRUNCATE TABLE BaremeNoteGroupeCours;
 
-DELETE TABLE GroupeCours;
+TRUNCATE TABLE GroupeCours;
 
-DELETE TABLE InscriptionGroupeCours;
+TRUNCATE TABLE InscriptionGroupeCours;
 
-DELETE TABLE ElementsEvaluation;
+TRUNCATE TABLE ElementsEvaluation;
 
-DELETE TABLE StatsEvaluation;
+TRUNCATE TABLE StatsEvaluation;
 
-DELETE TABLE ResultatEvaluation;
+TRUNCATE TABLE ResultatEvaluation;
 
 
-INSERT INTO Departement(idDepartement, identifiant, nomDepartement, nomFaculte) VALUES (seqDepartement.nextval, 7316012015, 'DÃ©partement d`informatique de l`UQAM', 'UQAM');
+INSERT INTO Departement(idDepartement, identifiant, nomDepartement, nomFaculte) VALUES (seqDepartement.nextval, 7316012439, 'Département d`informatique de l`UQAM', 'UQAM');
 
-INSERT INTO Etudiant(idEtudiant, codePermanent,  nip, nom, prenom, telephone, courriel, nbConnexionsInfructueuses, dateDerniereConnexion, idDepartement)VALUES(seqEtudiant.nextval, 'MANR02519207', '123456', 'Manori', 'Raymond', '5142270935', 'raymondM@uqam.ca', 0, SYSDATE, seqDepartement.currval);
-INSERT INTO Etudiant(idEtudiant, codePermanent,  nip, nom, prenom, telephone, courriel, nbConnexionsInfructueuses, dateDerniereConnexion, idDepartement)VALUES(seqEtudiant.nextval, 'TREL02519207', '113446', 'Tremblay', 'Lara', '5142298765', 'lara021@uqam.ca', 0, SYSDATE, seqDepartement.currval);
+INSERT INTO Etudiant(idProgramme, codePermanent,  nip, nom, prenom, telephone, courriel, nbConnexionsInfructueuses, dateDerniereConnexion, idProgramme)VALUES(seqEtudiant.nextval, 'MANR02519207', '123456', 'Manori', 'Raymond', '5142270935', 'raymondM@uqam.ca', 0, SYSDATE, seqDepartement.currval);
+INSERT INTO Etudiant(idProgramme, codePermanent,  nip, nom, prenom, telephone, courriel, nbConnexionsInfructueuses, dateDerniereConnexion, idProgramme)VALUES(seqEtudiant.nextval, 'TREL02519207', '113446', 'Tremblay', 'Lara', '5142298765', 'lara021@uqam.ca', 0, SYSDATE, seqDepartement.currval);
 
 --INSERT INTO EmployeDepartement(idEmploye, codeMS,  motDePasse, nom, prenom, telephone, courriel, idDepartement)VALUES(seqEmployeDepartement.nextval, 'hg9912423111', '00123456', 'Chieze', 'Emmanuel', '5149264532', 'chiezeEmmanuel@uqam.ca', seqDepartement.currval);
 --INSERT INTO EmployeDepartement(idEmploye, codeMS,  motDePasse, nom, prenom, telephone, courriel, idDepartement)VALUES(seqEmployeDepartement.nextval, 'ml9912098911', '00120996', 'Berger', 'Jacques', '5149269880', 'jacquesBerger@uqam.ca', seqDepartement.currval);
 
-INSERT INTO Enseignant(idEnseignant, codeMS,  motDePasse, nom, prenom, telephone, courriel) VALUES(seqEnseignant.nextval, 'hg9912423122', '12344456', 'Privat', 'Jean', '5142668929', 'privatJean@uqam.ca');
-INSERT INTO Enseignant(idEnseignant, codeMS,  motDePasse, nom, prenom, telephone, courriel) VALUES(seqEnseignant.nextval,'ml9912098911', '00120996', 'Berger', 'Jacques', '5149269880', 'jacquesBerger@uqam.ca');
+INSERT INTO Enseignant(idEnseignant, codeMS,  motDePasse, nom, prenom, telephone, courriel) VALUES(seqEnseignant.nextval, 'hg9912423227', '12344456', 'Privat', 'Jean', '5142668929', 'privatJean@uqam.ca');
+INSERT INTO Enseignant(idEnseignant, codeMS,  motDePasse, nom, prenom, telephone, courriel) VALUES(seqEnseignant.nextval,'ml9912022917', '00120996', 'Berger', 'Jacques', '5149269880', 'jacquesBerger@uqam.ca');
 
-INSERT INTO SessionCours(idSessionCours, nomSession, dateDebut, dateFin, dateLimiteRemiseNotes) VALUES (seqSessionCours.nextval, 'Automne2015', '2015-09-08 00:00:00', '2015-12-21 00:00:00', '2015-11-11 00:00:00');
+INSERT INTO SessionCours(idSessionCours, nomSession, dateDebut, dateFin, dateLimiteRemiseNotes) VALUES (seqSessionCours.nextval, 'Automne2012', '2015-09-08 00:00:00', '2015-12-21 00:00:00', '2015-11-11 00:00:00');
 
-INSERT INTO Cours(idCours, sigleCours, titreCours, descriptionCours, idDepartement) VALUES (seqCours.nextval,'INF3105', 'Structures de donnÃ©es et algorithmes', 'Graphes, arbres, tables..',seqDepartement.currval);
-INSERT INTO Cours(idCours, sigleCours, titreCours, descriptionCours, idDepartement) VALUES (seqCours.nextval,'INF4170', 'Architecture des ordinateurs', 'MÃ©moires: architecture,fonctionnement,types..',seqDepartement.currval);
+INSERT INTO Cours(idCours, sigleCours, titreCours, descriptionCours, idDepartement) VALUES (seqCours.nextval,'INF3105', 'Structures de données et algorithmes', 'Graphes, arbres, tables..',seqDepartement.currval);
+INSERT INTO Cours(idCours, sigleCours, titreCours, descriptionCours, idDepartement) VALUES (seqCours.nextval,'INF4170', 'Architecture des ordinateurs', 'Memoires: architecture,fonction...',seqDepartement.currval);
 
 --INSERT INTO BaremeNoteGroupeCours(idBaremeNoteGC, noteLettree, noteSeuil) VALUES (seqBaremeNoteGC.nextval, 'A+', 95);
 --INSERT INTO BaremeNoteGroupeCours(idBaremeNoteGC, noteLettree, noteSeuil) VALUES (seqBaremeNoteGC.nextval, 'A', 90);
@@ -61,11 +59,10 @@ INSERT INTO Cours(idCours, sigleCours, titreCours, descriptionCours, idDeparteme
 INSERT INTO GroupeCours(idGroupeCours, numeroGroupe, dateConfirmation, dateApprobation, transfertNotes, diffusionNotesFinales, idCours, idSessionCours, idBaremeNoteGC, idEnseignant) VALUES (seqGroupeCours.nextval, 20, '2015-05-15 00:00:00', '2015-05-01 00:00:00', 'O', 'O', seqCours.currval, seqSessionCours.currval, seqBaremeNoteGC.currval, 1);
 INSERT INTO GroupeCours(idGroupeCours, numeroGroupe, dateConfirmation, dateApprobation, transfertNotes, diffusionNotesFinales, idCours, idSessionCours, idBaremeNoteGC, idEnseignant) VALUES (seqGroupeCours.nextval, 30, '2015-05-15 00:00:00', '2015-05-01 00:00:00', 'O', 'O', seqCours.currval, seqSessionCours.currval, seqBaremeNoteGC.currval, 2);
 
-INSERT INTO Programme (idProgramme, codeNumerique, titreProgramme, typeProgramme, cycleProgramme, idDepartement) VALUES (seqProgramme.nextval, '7316' , 'GÃ©nie Logiciel',  'Informatique', 1, seqDepartement.currval);
+INSERT INTO Programme (idProgramme, codeNumerique, titreProgramme, typeProgramme, cycleProgramme, idDepartement) VALUES (seqProgramme.nextval, '7316' , 'Génie Logiciel', 1, 7316 ,seqDepartement.currval);
 
 --INSERT INTO InscriptionGroupeCours(idInscriptionGC, ordre, noteNumerique, noteLettree, idProgramme, idEtudiant, idStatutInscription, idGroupeCours) VALUES (seqInscriptionGC.nextval, 1, 95, 'A+', seqProgramme.currval, seqEtudiant.currval, seqStatutInscription.currval, seqGroupeCours.currval);
 
 INSERT INTO ElementsEvaluation(idElementsEvaluation, titreEvaluation, ordreApparition, resultatMax, ponderation, saisieEvaluation, transfertEvaluation, diffusion, idGroupeCours, idIncriptionGC) VALUES (seqElementsEvaluation.nextval, 'INF3105 TP1', 1, 89, 15, '2015-10-08 00:00:00', '2015-10-08 00:00:00', 'O', seqGroupeCours.currval, seqInscriptionGC.currval);
 
 COMMIT;
-
